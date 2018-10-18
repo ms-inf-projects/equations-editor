5750dfaa699945fa8775998110d10fb74a254624
Szymon Gibala <szymon.gibala@sap.com>
2018-10-18 15:50:24 +0200

    Progress save

--- src/modules/alphanumeric.js
+++ src/modules/alphanumeric.js
@@ -1,5 +1,6 @@
 module.exports = {
     isLiteralSymbol: function (keyCode) {
+        // TODO - while holding shit it is possible to enter !, @ etc.
         // 48-57 -> 0-9, 65-90 -> A-Z, 97-122 -> a-z
         if ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122))
             return true
